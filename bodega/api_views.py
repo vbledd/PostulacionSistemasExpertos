20cfe9d99dd5c785f2ff839967466900579d1c9e
vbledd <83681584+vbledd@users.noreply.github.com>
2022-01-21 09:50:36 -0300

    comentarios

--- bodega/api_views.py
+++ bodega/api_views.py
@@ -8,13 +8,13 @@ from .models import *
 from django.contrib.auth.hashers import make_password, check_password
 from django.db.models import Sum
 
-
+#creamos una funcion que nos permite formatear los datos de la bd a JSON
 def FormatoJson(consulta):
     respuesta = serializers.serialize("json", consulta, use_natural_foreign_keys=True)
     
     return respuesta
 
-
+#esta vista nos retorna todos los productos existentes en la bd
 class productosListar(APIView):
 
     def get(self, request, format=None):
@@ -27,6 +27,8 @@ class productosListar(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#esta vista nos retorna los productos a traves de un parametro de busqueda y de esta forma filtra los
+#datos segun el nombre ingresado en el parametro 'buscar'
 class productosBuscador(APIView):
 
     def get(self, request, format=None):
@@ -39,6 +41,7 @@ class productosBuscador(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#esta vista nos retorna los datos de un producto en particular el cual es filtrado a traves de su id
 class productosDatos(APIView):
 
     def get(self, request, format=None):
@@ -52,7 +55,7 @@ class productosDatos(APIView):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-
+#se reciben los parametros codigo,nombre,detalle,valor para crear el producto en la bd
 class productosCrear(APIView):
 
     def post(self, request, format=None):
@@ -73,6 +76,8 @@ class productosCrear(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#se reciben los parametros id,codigo,nombre,detalle,valor para modificar el producto en la bd
+#ademas de comprobar que no exista un codigo duplicado
 class productosEditar(APIView):
 
     def post(self, request, format=None):
@@ -95,6 +100,7 @@ class productosEditar(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#recibe por parametro el id del producto para eliminarlo de la bd
 class productosEliminar(APIView):
 
     def post(self, request, format=None):
@@ -107,6 +113,7 @@ class productosEliminar(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#lista todas las bodegas existentes en la bd
 class bodegasListar(APIView):
 
     def get(self, request, format=None):
@@ -119,6 +126,7 @@ class bodegasListar(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#muestra los datos especificos de una bodega a traves de su id
 class bodegaDatos(APIView):
 
     def get(self, request, format=None):
@@ -131,6 +139,8 @@ class bodegaDatos(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#se reciben los parametros nombre,direccion para crear una bodega en la bd
+#ademas de comprobar si el nombre esta duplicado
 class bodegasCrear(APIView):
 
     def post(self, request, format=None):
@@ -147,6 +157,7 @@ class bodegasCrear(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#se reciben los parametros nombre, direccion para editar la informacion en la bd y comprueba los nombres duplicados
 class bodegasEditar(APIView):
 
     def post(self, request, format=None):
@@ -169,6 +180,7 @@ class bodegasEditar(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#se recibe el id para eliminar una bodega de la bd
 class bodegasEliminar(APIView):
 
     def post(self, request, format=None):
@@ -182,6 +194,7 @@ class bodegasEliminar(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#lista los productos que contienen todas las bodegas
 class bodegasProductosListar(APIView):
 
     def get(self, request, format=None):
@@ -195,6 +208,7 @@ class bodegasProductosListar(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#lista los productos de una bodega en especifico a traves de la id de la bodega
 class bodegasListarProductos(APIView):
 
     def get(self, request, format=None):
@@ -209,6 +223,9 @@ class bodegasListarProductos(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#modifica el stock de un producto obteniendo el id de la bodega y el id del producto
+#para modificar o agregar stock al producto,esto comprueba si existe un producto en la bodega
+#si este existe modifica el stock, de no existir agrega el producto a la bodega con el stock enviado
 class AgregarStock(APIView):
 
     def post(self, request, format=None):
@@ -240,6 +257,7 @@ class AgregarStock(APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+#nos muestra los datos de un producto especifico de una bodega recibiendo el id de la bodega y el id del producto
 class BodegaProductoEspecifico(APIView):
 
     def get(self, request, format=None):
@@ -261,7 +279,7 @@ class BodegaProductoEspecifico(APIView):
 
 
 
-
+#nos lista los ultimos 10 productos ingresados
 class resumenProductos(APIView):
 
     def get(self, request, format=None):
@@ -275,7 +293,9 @@ class resumenProductos(APIView):
         except Exception as e:
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
+            
+#nos devuelve datos sobre la cantidad de productos ingresados en cada bodega y el stock total sumado de
+#todas las bodegas
 class resumenBodegas(APIView):
 
     def get(self, request, format=None):
--- bodega/views.py
+++ bodega/views.py
@@ -8,30 +8,33 @@ from django.contrib.auth.hashers import make_password, check_password
 
 @requiereLogin
 def index(request):
-
+    #renderiza el html 
     return render(request, 'paginas/index.html')
 
 
 def loginForm(request):
+    # utilizamos uun try para controlar excepciones
     try:
+        #si el usuario tiene iniciada la sesion se redirecciona
+        #al index, de no ser asi renderiza el html que contiene el login
         if request.session['login']:
             return redirect('index')
     except:
         return render(request, 'login.html')
 
 def login(request):
+    #guardamos en variables los parametros post del request
     usuarioEmail = request.POST.get('email')
     password = request.POST.get('pass')
 
-    print(f"{usuarioEmail}, {password}")
     
     try: 
+        #buscamos en la bd si existe un usuario con el email ingresado
+        #en caso de no existir nos mostrara un mensaje de error a traves del except
         registro = usuario.objects.get(email = usuarioEmail)
-        print(registro.password)
-        print(check_password(password,registro.password))
-
+        #comprobamos la contraseña ingresada con el hash de la contraseña en la bd
         if check_password(password,str(registro.password)):
-           
+           #si esta todo ok guardamos los datos del usuario en la session
             datos = {
                 'id' : registro.id,
                 'email' : registro.email,
@@ -48,24 +51,28 @@ def login(request):
 
 @requiereLogin
 def logout(request):
-
+    #eliminamos la sesion del usuario
     del request.session['login']
     return redirect('login-formulario')
 
 @requiereLogin
 def productos(request):
+    #renderiza el html de productos
     return render(request, 'paginas/productos.html')
 
 @requiereLogin
 def bodegas(request):
+    #renderiza el html de bodegas
     return render(request, 'paginas/bodegas.html')
 
 @requiereLogin
 def ingresoStock(request):
+    #renderiza el html de ingreso de stock
     return render(request, 'paginas/ingresoStock.html')
 
 @requiereLogin
 def bodegasProductos(request):
+    #renderiza el html quue contiene las bodegas
     id = request.GET.get('id')
     registro = bodega.objects.get(id=id)
     return render(request, 'paginas/bodegasProductos.html',{'bodega' : registro})
